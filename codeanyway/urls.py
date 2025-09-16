10c9b6887875e28f593a45129be810785353f552
MohaMed Nabil <mohamednabilpro2024@gmail.com>
2025-09-16 15:09:50 +0300

    add static and templates

--- codeanyway/urls.py
+++ codeanyway/urls.py
@@ -1,39 +1,20 @@
-"""
-URL configuration for codeanyway project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from django.urls import include # Import include to include app URLs
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.urls import re_path
 from django.conf.urls.i18n import i18n_patterns
-from django.conf.urls.i18n import set_language
 from django.utils.translation import gettext_lazy as _
 
 urlpatterns = [
+    # هنا Django بيضيف urls خاصة بتغيير اللغة (set_language وغيره)
     path('i18n/', include('django.conf.urls.i18n')),
-
 ]
-urlpatterns = i18n_patterns(
-        path('admin/', admin.site.urls),
-)
 
+urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
+    path('', include('core.urls'), name='core'),
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
